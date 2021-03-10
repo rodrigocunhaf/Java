488e426afaab31af34b0968418d3201d7ac992f6
rodrigocunhaf <rodrigo.c.f@outlook.com>
2021-03-10 19:25:09 -0300

    For Challenge

--- Java Control Structures/ForChallenge/src/forchallenge/ForChallengeMain.java
+++ Java Control Structures/ForChallenge/src/forchallenge/ForChallengeMain.java
@@ -1,10 +1,10 @@
 package forchallenge;
 
 /*
- String  word  =  "#";
-		for (int i = 1; i<= 5;i++) {
-			System.out.println(word);
-			word+="#";
+String  word  =  "#";
+for (int i = 1; i<= 5;i++) {
+	System.out.println(word);
+	word+="#";
  */
 
 public class ForChallengeMain {
