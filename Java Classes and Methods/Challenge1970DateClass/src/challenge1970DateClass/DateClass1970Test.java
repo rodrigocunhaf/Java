723d933013d8b3ac395a02144434a2c5fe614dc9
rodrigocunhaf <rodrigo.c.f@outlook.com>
2021-03-11 18:00:57 -0300

    Challenge Date with 2 Constructors

--- Java Classes and Methods/Challenge1970DateClass/src/challenge1970DateClass/DateClass1970Test.java
+++ Java Classes and Methods/Challenge1970DateClass/src/challenge1970DateClass/DateClass1970Test.java
@@ -6,10 +6,14 @@ public class DateClass1970Test {
 		
 		//Create date #1 [Constructor #1]
 		DateClass1970 date = new DateClass1970();
+		
+		//Print Date #1
 		System.out.println(date.getDate());
 		
 		//Create date #2 [Constructor #2]
 		DateClass1970 date2 = new DateClass1970(11,03, 2021);
+		
+		//Print date #2
 		System.out.println(date2.getDate());
 		
 		
