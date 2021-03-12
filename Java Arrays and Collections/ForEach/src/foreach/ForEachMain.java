50400507779c03d7646097b501b72c8861f64931
rodrigocunhaf <rodrigo.c.f@outlook.com>
2021-03-12 09:53:13 -0300

    Foreach exemple

---
+++ Java Arrays and Collections/ForEach/src/foreach/ForEachMain.java
@@ -0,0 +1,15 @@
+package foreach;
+
+//Foreach exemple
+public class ForEachMain {
+	public static void main(String[] args) {
+		
+		//Array
+		double [] arr = {1.1,1.2,1.3,1.4};
+		
+		//Foreach
+		for(double num:arr) {
+			System.out.print(num+" ");
+		}
+	}
+}
